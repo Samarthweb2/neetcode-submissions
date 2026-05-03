17da9203fb1c8e8fa23531de1efaa34862e1335d
samarth@ML <167510798+Samarthweb2@users.noreply.github.com>
2026-05-03 20:03:54 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+
+        
+        ListNode* head = NULL;
+        if (l1->val <= l2->val) {
+            head = l1;
+            l1 = l1->next;
+        } else {
+            head = l2;
+            l2 = l2->next;
+        }
+
+        ListNode* tail = head;
+
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+
+        tail->next = (l1 ? l1 : l2);
+        return head;
+    }
+};
