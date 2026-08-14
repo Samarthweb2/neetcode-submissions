4097af3754de57549321ee1d8ec11ca88bbf832e
sam <167510798+Samarthweb2@users.noreply.github.com>
2026-08-14 07:42:40 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,15 @@
+#greedy algo we are using here 
+#Our approach- O(n)
+
+
+class Solution:
+    def maxProfit(self, prices):
+        min_price = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = price - min_price
+            max_profit = max(max_profit, profit)
+
+        return max_profit
